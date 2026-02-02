899d741bc5f2d6afc01019777643e19bbce0c813
Rohan Chakraborty <127221911+MR-IRRESPECTIVE@users.noreply.github.com>
2026-02-02 13:03:43 +0530

    Change route to catch all paths and redirect to Index

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,9 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
           <Route path="/memories" element={<Memories />} />
+          <Route path="/*" element={<Index />} />
+
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
